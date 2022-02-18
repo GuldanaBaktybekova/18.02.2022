9cdf6db23e4e0a1bb1c44518d558ac23582d4824
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-18 15:36:22 +0600

    Added borderStyle to script.js

--- script.js
+++ script.js
@@ -22,6 +22,55 @@ let range = document.querySelector('#font-size');
 
   range.addEventListener('input', function(event){
   example1h1.style.fontSize = range.value + 'px';
+  // range.nextSibling.textContent = example1h1.style.fontSize;
+});
+
+
+
+let bold = document.querySelector('#bold');
+let italic = document.querySelector('#italic');
+let underline = document.querySelector('#underline');
+
+  bold.addEventListener('input', function(event){
+  example1h1.style.fontWeight = bold.checked ? 'bold' : 'normal';
+ 
   
 });
+  italic.addEventListener('input', function(event){
+  example1h1.style.fontStyle = italic.checked ? 'italic' : 'normal';
+ 
+  
+});
+
+underline.addEventListener('input', function(event){
+  example1h1.style.textDecoration = underline.checked ? 'underline' : 'none';
+
+ 
+  
+});
+
+
+document.querySelector('#none').addEventListener('input', function(){
+  example1h1.style.borderStyle = "none";
+
+});
+document.querySelector('#solid').addEventListener('input', function(){
+  example1h1.style.borderStyle = "solid";
+
+});
+document.querySelector('#dashed').addEventListener('input', function(){
+  example1h1.style.borderStyle = "dashed";
+
+});
+document.querySelector('#dotted').addEventListener('input', function(){
+  example1h1.style.borderStyle = "dotted";
+
+});
+
+
+
+
+
+
+
 
