899baff54713baf1166b442c86bf5941130e3e25
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-18 14:43:47 +0600

    Added font to script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let example1 = document.querySelector('#example1');
 let example1h1 = document.querySelector('#example1h1');
-  example1.addEventListener('keydown', function(event){
+  example1.addEventListener('input', function(event){
   example1h1.textContent = example1.value;
  
+});
+let example2 = document.querySelector('#example2');
+let example3 = document.querySelector('#example3');
+  example2.addEventListener('input', function(event){
+  example1h1.style.color = example2.value;
+  example1h1.style.backgroundColor = example3.value;
+});
+
+
+let font = document.querySelector('#font');
+
+  font.addEventListener('input', function(event){
+  example1h1.style.fontFamily = font.value;
+  
 });
