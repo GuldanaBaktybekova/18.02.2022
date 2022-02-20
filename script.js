7005338db60563dcbc111f12e534ab00bbeba71d
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-02-20 22:58:53 +0600

    Added a url to script.js

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
+// let h1 = document.querySelector('h1');
+
+// document.querySelector('#text').addEventListener('input', function() {
+//   h1.textContent = this.value;
+// });
+// document.querySelector('#color').addEventListener('input', function() {
+//   h1.style.color = this.value;
+// });
+// document.querySelector('#bg-color').addEventListener('input', function() {
+//   h1.style.backgroundColor = this.value;
+// });
+// document.querySelector('#font-family').addEventListener('input', function() {
+//   h1.style.fontFamily = this.value;
+// });
+// document.querySelector('#font-size').addEventListener('input', function() {
+//   h1.style.fontSize = this.value + 'px';
+//   this.nextSibling.textContent = h1.style.fontSize;
+// });
+// document.querySelector('#bold').addEventListener('input', function() {
+//   if (this.checked) {
+//     h1.style.fontWeight = 'bold';
+//   }
+//   else {
+//     h1.style.fontWeight = 'normal';
+//   }
+// });
+// document.querySelector('#italic').addEventListener('input', function() {
+//   h1.style.fontStyle = this.checked ? 'italic' : 'normal';
+// });
+// document.querySelector('#underline').addEventListener('input', function() {
+//   h1.style.textDecoration = this.checked ? 'underline' : 'none';
+// });
+// document.getElementsByName('border').forEach(function(input) {
+//   input.addEventListener('input', function() {
+//     h1.style.border = `1px ${this.value} black`;
+//   });
+// });
+// document.querySelector('#url').addEventListener('input', function() {
+//   h1.innerHTML = this.value ? `<a href="${this.value}">${h1.textContent}</a>` : h1.textContent;
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let example1 = document.querySelector('#example1');
 let example1h1 = document.querySelector('#example1h1');
   example1.addEventListener('input', function(event){
@@ -67,6 +136,14 @@ document.querySelector('#dotted').addEventListener('input', function(){
 
 });
 
+//Short
+// document.getElementsByName('border').forEach(function(input){
+//   input.addEventListener('input', function(){
+//     example1h1.style.border = `1px ${this.value} black`;
+// });
+document.querySelector('#url').addEventListener('input', function() {
+  example1h1.innerHTML = this.value ? `<a href="${this.value}">${example1h1.textContent}</a>` : example1h1.textContent;
+});
 
 
 
